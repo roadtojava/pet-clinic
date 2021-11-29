8901a8b9997480da260e777911b50fb6118b1046
Nihad <nihad.akbarli@idrak.az>
2021-11-29 18:15:31 +0400

    intial spring commit

---
+++ pet-clinic-web/test/java/guru/springframework/pet/PetApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
